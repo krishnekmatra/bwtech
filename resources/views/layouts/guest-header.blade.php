a9d2338083808e8492b239efa2dc6fa7e0a1046d
krishna patel <krishnapatel.santophy@gmail.com>
2023-06-13 10:50:23 +0530

    small changes in header

--- resources/views/layouts/guest-header.blade.php
+++ resources/views/layouts/guest-header.blade.php
@@ -2,7 +2,7 @@
 			<div class="header-top">
 				<div class="container">
 					<div class="header-left">
-						<p class="welcome-msg">Welcome to Bwtech Store!</p>
+						<p class="welcome-msg">Welcome to Bwtech Store !</p>
 					</div>
 					<div class="header-right">
 					   
